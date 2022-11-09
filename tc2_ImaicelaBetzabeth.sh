595e2826e8e53932428245dc576d6733a0b9f5b7
ImaicelaBetzabethBiotech <117690624+ImaicelaBetzabethBiotech@users.noreply.github.com>
2022-11-09 15:37:42 -0500

    Se ha subido el Bash Script 1
    Realizado por Betzabeth Imaicela

---
+++ tc2_ImaicelaBetzabeth.sh
@@ -0,0 +1,8 @@
+echo -e "\n"   #Este comando designa saltos de línea
+echo "Estas son las primeras 7 filas" #Esta línea indica un mensaje de separación  
+head -n 7 EigenBetw.tsv #Este comando me permite visualizar las primeras 7 líneas
+echo -e "\n"
+echo "Estas son las últimas 5 filas"  #Esta línea indica un mensaje de separación
+tail -n 5 EigenBetw.tsv  #Este comando me permite visualizar las úñtimas 5 líneas
+
+
